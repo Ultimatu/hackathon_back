96e01c4e299f1ac921946a26dd848dbb05a68b04
Tonde Souleymane <tondesoulco@gmail.com>
2023-08-19 20:33:23 +0000

    update langs

---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'required' => 'Le champ :attribute est requis.',
+    'email' => 'Le champ :attribute doit être une adresse e-mail valide.',
+    'unique' => 'La valeur du champ :attribute est déjà utilisée.',
+];
